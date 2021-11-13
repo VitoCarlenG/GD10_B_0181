893f1cc267ba9b0706c9d0741a6a70c27bd37f58
VitoCarlenG <xlhvitocarlen@gmail.com>
2021-11-13 22:04:10 +0700

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,11 @@
 import firebase from 'firebase/app'
 import 'firebase/database'
 
+export const db = firebase
+    .initializeApp({databaseURL: 'https://guidedvuefire0181-default-rtdb.asia-southeast1.firebasedatabase.app/'})
+    .database()
 
+export const drinkRef = db.ref('drinks')
 
 
 
--- src/views/Drinks.vue
+++ src/views/Drinks.vue
@@ -110,6 +110,7 @@
 
 <script>
 //tambahkan ini untuk import database reference
+import { drinkRef } from "../firebase";
 
 export default {
   data: () => ({
@@ -149,6 +150,9 @@ export default {
   }),
 
   //tambahkan attribute firebase disini
+  firebase: {
+    drinks: drinkRef,
+  },
 
   computed: {
     formTitle() {
@@ -179,7 +183,17 @@ export default {
     },
 
     //tambahkan ini untuk delete data
-    deleteItemConfirm() {},
+    deleteItemConfirm() {
+      //tambahkan ini untuk delete data
+      drinkRef
+        .child(this.editedIndex)
+        .remove()
+        .then(() => {
+          alert("Berhasil Hapus Data !");
+        })
+        .catch((err) => [alert("Gagal Hapus Data: ", err)]);
+      this.closeDelete();
+    },
 
     close() {
       this.dialog = false;
@@ -198,7 +212,27 @@ export default {
     },
 
     // Tambahkan code method save
-    save() {},
+    save() {
+      if (this.editedIndex != -1) {
+        //tambahkan ini untuk edit data
+        drinkRef
+          .child(this.editedIndex)
+          .set(this.editedItem)
+          .then(() => {
+            alert("Berhasil Edit Data !");
+          })
+          .catch((err) => [alert("Gagal Edit Data: ", err)]);
+      } else {
+        //tambahkan code ini untuk create data ke db
+        drinkRef
+          .push(this.editedItem)
+          .then(() => {
+            alert("Berhasil Tambah Data !");
+          })
+          .catch((err) => [alert("Gagal Tambah Data: ", err)]);
+      }
+      this.close();
+    },
   },
 };
 </script>
